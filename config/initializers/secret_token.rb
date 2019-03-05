3cc4deab637f66485ef6c7948f1967597a1c376f
Eric-Guo <eric.guocz@gmail.com>
2019-03-05 23:42:33 +0800

    Remove api/v1 namespace and make it work with Eric-Guo/oauth2id

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-DoorkeeperDeviseClient::Application.config.secret_token = '4c8e2af4d0a13797cbedba84c7f2908a5bb901e7408614cfe693be5b6580539b676e07f065c353de851fb0ee54bb0864db28d8643c46875657e9143175c0fc21'
+DoorkeeperDeviseClient::Application.config.secret_key_base = '4c8e2af4d0a13797cbedba84c7f2908a5bb901e7408614cfe693be5b6580539b676e07f065c353de851fb0ee54bb0864db28d8643c46875657e9143175c0fc21'
